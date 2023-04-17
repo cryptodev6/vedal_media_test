96b7ec4be30d21e9cd5e5e63aa8811a91502ccf7
Crypto Dev <herts92@hotmail.com>
2023-04-17 09:06:28 -0500

    popup added

--- vedal-media-test/src/app/document-viewer/document-viewer.component.ts
+++ vedal-media-test/src/app/document-viewer/document-viewer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { DocumentService } from '../shared/services/document.service';
 import { ActivatedRoute } from '@angular/router';
 
@@ -8,14 +8,35 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./document-viewer.component.scss']
 })
 export class DocumentViewerComponent implements OnInit {
-  constructor(private route: ActivatedRoute) {
-    this.route.params.subscribe(params => {
-      console.log(params['id']);
-    });
+  position: { x: number, y: number } = { x: 0, y: 0 };
+  isPopupOpen = false;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {}
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    /*if (this.isPopupOpen) {
+      const popup = document.querySelector('.popup') as HTMLElement;
+      const isClickInsidePopup = popup.contains(event.target as Node);
+
+      if (!isClickInsidePopup) {
+        this.closePopup();
+      }
+    }*/
   }
 
-  ngOnInit(): void {
+  showPopup(event: MouseEvent) {
+    console.log("show popup", event);
+    this.position.x = event.clientX;
+    this.position.y = event.clientY;
+    this.isPopupOpen = true;
+    console.log("isPopupOpen", this.isPopupOpen);
+  }
 
+  closePopup() {
+    this.isPopupOpen = false;
   }
 
   zoomIn() {
